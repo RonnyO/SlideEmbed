0abd05ec1dbec15b1be433d9dca3eb7ff165647c
Ronny Orbach <r@ronnyo.com>
2011-06-22 02:45:41 +0300

    split to multiple files

---
+++ SlideEmbed.js
@@ -0,0 +1,53 @@
+$(function(){
+ 
+ 
+(function($){
+    var $controls = $('<ul class="embedControls clearfix"> \
+                          <li class="fullscreenBtn"><a href="#">Fullscreen</a></li>\
+                      </ul>'),
+        popup,
+        inFullscreenMode = function(){
+            return /fullscreen=true/.test(document.location.href);
+        },
+        goFullscreen = function(){
+            console.log('goFullscreen');
+            popup = window.open(
+                document.location.href + '?fullscreen=true',
+                null,
+                'left=0,\
+                top=0,\
+                width=' + screen.availWidth + '\
+                height=' + screen.availHeight + '\
+                resizable=no,\
+                menubar=no,\
+                toolbar=no,\
+                location=no,\
+                personalbar=no,\
+                status=no,\
+                scrollbars=no'
+            );
+        },
+        exitFullscreen = function(){
+            console.log('exitFullscreen');
+            window.close();
+        };
+            
+    window.injectControls = function(){
+        $('body').append($controls);
+    };
+    
+    
+    $('a', $controls).click(function(){
+        if (inFullscreenMode()) {
+            exitFullscreen()
+        } else {
+            goFullscreen()
+        }
+    });
+})(jQuery);
+ 
+ 
+ 
+injectControls();
+ 
+});
